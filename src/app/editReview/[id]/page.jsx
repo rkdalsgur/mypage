2385f21f6663695021480502bd7a7532a94177a0
rkdalsgur <127273488+rkdalsgur@users.noreply.github.com>
2023-12-09 23:21:15 +0900

    maybe last'

--- src/app/editReview/[id]/page.jsx
+++ src/app/editReview/[id]/page.jsx
@@ -1,4 +1,3 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import EditReviewForm from '@/components/EditReviewForm'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
