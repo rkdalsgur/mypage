d3a8bed14007a6ee423a6670dfe23a0e1fe84e12
rkdalsgur <127273488+rkdalsgur@users.noreply.github.com>
2023-12-10 20:51:04 +0900

    hihi

--- src/app/review/page.jsx
+++ src/app/review/page.jsx
@@ -12,12 +12,7 @@ export default function Home() {
           </p>
         </Link>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex   place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="pointer-events-none flex   place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
             By 정보보호학과 92014942 강민혁
           </a>
         </div>
@@ -43,9 +38,7 @@ export default function Home() {
           리뷰 추가하기
         </Link>
       </div>
-      <div>
-        <Reviewlist />
-      </div>
+      <div>{/* <Reviewlist /> */}</div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
     </main>
