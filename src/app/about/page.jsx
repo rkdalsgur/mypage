87b66b4a12ce159c355011a449b94919e5352c4a
rkdalsgur <127273488+rkdalsgur@users.noreply.github.com>
2023-12-10 21:41:17 +0900

    hi2

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,32 +1,29 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col lg:flex-row">
         <a
           href="../"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 mb-4 lg:mb-0"
           rel="noopener noreferrer"
         >
           <p className={`mb-3 text-xl font-semibold`}>HOME </p>
         </a>
-        <div>
-          <h2 className="text-center text-2xl">자기 소개</h2>
+        <div className="text-center lg:text-left mb-4 lg:mb-0">
+          <h2 className="text-2xl">자기 소개</h2>
           <br />
           <br />
-          <h2 className="text-center text-xl">생년월일: 2001.02.20</h2>
+          <h2 className="text-xl">생년월일: 2001.02.20</h2>
           <br />
-          <h2 className="text-center text-xl">학번: 92014942</h2> <br />
-          <h2 className="text-center text-xl">전공: 정보보호학과</h2>
+          <h2 className="text-xl">학번: 92014942</h2> <br />
+          <h2 className="text-xl">전공: 정보보호학과</h2>
           <br />
-          <h2 className="text-center text-xl">연락처: 010-5772-0285</h2>
+          <h2 className="text-xl">연락처: 010-5772-0285</h2>
         </div>
-        <br />
-        <br />
-        <br />
-        <div>
+        <div className="mb-4 lg:mb-0">
           <a
             href="/review"
-            className="group rounded-lg border  text-center border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+            className="group rounded-lg border text-center border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -37,11 +34,11 @@ export default function Home() {
             </h2>
           </a>
         </div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <p className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
-            By 정보보호학과 92014942 강민혁
-          </p>
-        </div>
+      </div>
+      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <p className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
+          By 정보보호학과 92014942 강민혁
+        </p>
       </div>
     </main>
   )
--- src/app/review/page.jsx
+++ src/app/review/page.jsx
@@ -5,14 +5,17 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="relative z-10 max-w-5xl w-full font-mono text-sm lg:flex lg:justify-between">
+        {/* Header */}
         <Link href="../">
-          <p className="fixed left-0 top-0 flex w-full font-semibold justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <p className="fixed left-0 top-0 w-full font-semibold justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             안녕하세요 강민혁의 개인 페이지입니다.&nbsp;
           </p>
         </Link>
+
+        {/* Footer */}
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a className="pointer-events-none flex   place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
+          <a className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
             By 정보보호학과 92014942 강민혁
           </a>
         </div>
@@ -38,7 +41,9 @@ export default function Home() {
           리뷰 추가하기
         </Link>
       </div>
-      <div>{/* <Reviewlist /> */}</div>
+      <div>
+        <Reviewlist />
+      </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
     </main>
