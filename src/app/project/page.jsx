be65c0d186641b48e5de02213a2178f07ea8fed5
rkdalsgur <127273488+rkdalsgur@users.noreply.github.com>
2023-12-11 12:26:46 +0900

    latest

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="group rounded-lg border border-transparent px-5 py-4 transition-colors">
-        <h2 className="mb-3 text-2xl font-semibold">My Projects</h2>
-      </p>
+      <h2 className="mb-3 text-2xl font-semibold">My Projects</h2>
+
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -28,7 +27,7 @@ export default function Home() {
         />
       </div> */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-60 grid text-center lg:max-w-5xl lg:w-full lg:mb-50 lg:grid-cols-4 lg:text-left">
         <a
           href="https://myth-green.vercel.app/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
